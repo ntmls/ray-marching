defbb4b40c06756a7f2a97f4123444875df9566c
Nathan Miles <ntmls@live.com>
2021-09-06 11:16:33 -0500

    Add testing

---
+++ test/vector2.test.ts
@@ -0,0 +1,18 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+import { Vector2 } from '../src/Domain/2d/Vector2';
+
+const expect = chai.expect; 
+describe('Vector2', () => {
+
+  it('should calculate magnitude' , () => {
+    var v = new Vector2(1, 1);
+    expect(v.magnitude).to.be.closeTo(1.4142135623730951, .00001);
+  });
+
+  it('should have normalized vector with magnitude of 1' , () => {
+    var v = new Vector2(1, 1);
+    expect(v.normalize().magnitude).to.be.closeTo(1, .00001);
+  });
+
+});
